d7916bef0e70c31895d9c31b725c096a456b73be
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-09-06 13:00:10 +0200

    add ApiEndpoints class with base URL and task-related endpoints

---
+++ lib/core/api/api_endpoints.dart
@@ -0,0 +1,8 @@
+class ApiEndpoints {
+  static const String baseUrl = "http://localhost/BienenPlan/backend/public/api";
+  static const String login = "$baseUrl/login";
+  static const String tasks = "$baseUrl/tasks";
+
+  static String taskDetail(int id) => "$baseUrl/tasks/$id";
+  static String updateTaskStatus(int id) => "$baseUrl/tasks/$id/status";
+}
