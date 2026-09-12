cb3cfd30a9dbcfc93d6b6751bdaa27913d101aef
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-09-12 03:46:44 +0200

    feat: define TaskRepositoryContract for task management methods

--- lib/features/tasks/data/task_repository.dart
+++ lib/features/tasks/data/task_repository.dart
@@ -2,9 +2,15 @@ import '../../../core/api/api_client.dart';
 import '../../../core/api/api_endpoints.dart';
 import 'task_model.dart';
 
-class TaskRepository {
+abstract class TaskRepositoryContract {
+  Future<List<Task>> fetchTasks();
+  Future<void> updateTaskStatus(int taskId, String newStatus);
+}
+
+class TaskRepository implements TaskRepositoryContract {
   final ApiClient _apiClient = ApiClient();
 
+  @override
   Future<List<Task>> fetchTasks() async {
     final response = await _apiClient.get(ApiEndpoints.tasks);
 
@@ -15,6 +21,7 @@ class TaskRepository {
     }
   }
 
+  @override
   Future<void> updateTaskStatus(int taskId, String newStatus) async {
     await _apiClient.post(
       ApiEndpoints.updateTaskStatus(taskId),
