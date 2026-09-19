f349733b0d335b0044e0cd12527b1e6d2c3f19e1
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-09-19 20:20:47 +0200

    feat: add getBAseUrl for platform selecting, update meatdata for Andriod

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -1,15 +1,18 @@
+import 'package:flutter/foundation.dart';
+
 class ApiEndpoints {
-  static const String baseUrl =
-      "http://localhost/BienenPlan/backend/public/api";
-  static const String serverBaseUrl =
-      "http://localhost/BienenPlan/backend/public";
-  static const String login = "$baseUrl/login";
-  static const String register = "$baseUrl/register";
-  static const String me = "$baseUrl/me";
-  static const String tasks = "$baseUrl/tasks";
-  static const String projects = "$baseUrl/projects";
-  static const String containers = "$baseUrl/containers";
-  static const String groups = "$baseUrl/groups";
+  // getter statt static const
+  static String get baseUrl => "${getBaseUrl()}/BienenPlan/backend/public/api";
+  static String get serverBaseUrl =>
+      "${getBaseUrl()}/BienenPlan/backend/public";
+
+  static String get login => "$baseUrl/login";
+  static String get register => "$baseUrl/register";
+  static String get me => "$baseUrl/me";
+  static String get tasks => "$baseUrl/tasks";
+  static String get projects => "$baseUrl/projects";
+  static String get containers => "$baseUrl/containers";
+  static String get groups => "$baseUrl/groups";
 
   static String taskDetail(int id) => "$baseUrl/tasks/$id";
   static String updateTaskStatus(int id) => "$baseUrl/tasks/$id/status";
@@ -20,8 +23,24 @@ class ApiEndpoints {
   static String removeGroup(int taskId, int groupId) =>
       "$baseUrl/tasks/$taskId/groups/$groupId";
 
-  /// Baut die vollständige URL zu einem gespeicherten Anhang (relativer Pfad
-  /// wie z.B. "uploads/tasks/xyz.pdf") auf.
+  /// Baut die vollständige URL zu einem gespeicherten Anhang auf.
   static String attachmentUrl(String relativePath) =>
       "$serverBaseUrl/$relativePath";
+}
+
+String getBaseUrl() {
+  if (kIsWeb) {
+    return 'http://localhost'; // Für Chrome / Web
+  }
+
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.android:
+      return 'http://172.23.240.1';
+    case TargetPlatform.iOS:
+    case TargetPlatform.windows:
+    case TargetPlatform.macOS:
+    case TargetPlatform.linux:
+    case TargetPlatform.fuchsia:
+      return 'http://localhost';
+  }
 }
