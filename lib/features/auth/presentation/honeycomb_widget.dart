b1a2e7029f3e49b0109b7ada6fc39d36acecef05
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-09-20 21:22:02 +0200

    feat: add HoneycombBackground and AnimatedHoneycombWidget for animated background rendering

---
+++ lib/features/auth/presentation/honeycomb_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../animation/honeycomb_background.dart';
+
+// Kompatibilitäts-Wrapper für bestehende Aufrufe.
+// Die eigentliche Logik liegt jetzt in der separaten Hintergrund-Komponente.
+class AnimatedHoneycombWidget extends StatelessWidget {
+  const AnimatedHoneycombWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HoneycombBackground();
+  }
+}
