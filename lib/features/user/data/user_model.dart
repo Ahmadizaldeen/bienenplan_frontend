6f26f70d22ded8e48570e4116204e2bd733d26ed
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-09-21 20:38:23 +0200

    feat: add optional picture field to AppUser model

--- lib/features/user/data/user_model.dart
+++ lib/features/user/data/user_model.dart
@@ -2,14 +2,23 @@ class AppUser {
   final int id;
   final String name;
   final String email;
+  final String? picture;
 
-  AppUser({required this.id, required this.name, required this.email});
+  const AppUser({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.picture,
+  });
 
   factory AppUser.fromJson(Map<String, dynamic> json) {
     return AppUser(
-      id: json['id'] is int ? json['id'] as int : int.parse(json['id'].toString()),
+      id: json['id'] is int
+          ? json['id'] as int
+          : int.parse(json['id'].toString()),
       name: json['name'] as String,
       email: json['email'] as String,
+      picture: json['picture'] as String?,
     );
   }
 }
